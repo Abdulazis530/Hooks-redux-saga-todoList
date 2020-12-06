7a7e066debbad3826fe7d0e2ac7eeff13394c4a1
azis <abdlazis530@gmail.com>
2020-12-06 22:08:19 +0700

    removing unnecesary folders

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import Home from './pages/Home';
-import FormAddTodo from './containers/FormAddTodo'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import "./assets/styles/style.scss"
 
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/addtodo" component={FormAddTodo} />
-      </Switch>
-    </Router>
+    <>
+      <Home />
+    </>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Modal from './Modals'
+import Navbar from './Navbar'
+import SearchInput from './SearchInput'
+import TodoCategory from './TodoCategory'
 
-
-export default function Header({ children }) {
+export default function Header() {
+    const [openModal, setOpenModal] = useState(false)
     return (
-        <header>
-            {children}
+        <header className="header">
+            <Navbar />
+            <div className="header__container">
+                <div className="filter">
+                    <SearchInput />
+                </div>
+                <TodoCategory />
+                <button className="btn btn-add" onClick={() => setOpenModal(true)}>
+                    <i className="fas fa-plus fa-2x"></i>
+                </button>
+                <Modal isOpen={openModal} onClose={() => setOpenModal(false)} />
+            </div>
         </header>
     )
 }
